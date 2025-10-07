8f46c3e514b129ee98f0ef6a2a29b3b047cfa260
Tigas-Aulas <a67466@alunos.ipb.pt>
2025-10-07 11:37:17 +0100

    ex10

---
+++ ex10.py
@@ -0,0 +1,13 @@
+import random
+
+numero =random.randint(1,10)
+
+A=0
+tentativa=0
+
+while (A!=numero):
+    
+    A=int(input("insira um numero de 1 a 10\n"))
+    tentativa+=1
+
+print("o numero de tentativas foi",tentativa)
