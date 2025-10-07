578d19f16c2a2b2a6b61b4c6d431ca18aaa83f49
Tigas-Aulas <a67466@alunos.ipb.pt>
2025-10-07 10:01:34 +0100

    yah

--- ex7.py
+++ ex7.py
@@ -1,15 +1,15 @@
-at=float(input("insira a sua altura\n"))
+at=float(input("insira a sua altura em metros\n"))
 kg=int(input("insira o seu peso\n"))
 
 imc=kg/(at*at)
 
 if(imc<18.5):
     print("abaixo do peso")
-if(imc<=18.5 and imc<24.9):
+if(imc>=18.5 and imc<24.9):
     print("peso normal")
 if(imc>=25 and imc<29.9):
     print("excesso de peso")
 if(imc>30):
     print("obesidade")
 
-print(imc)
+print("o seu imc é de ",imc)
